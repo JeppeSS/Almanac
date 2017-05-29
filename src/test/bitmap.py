5b2ee6546c556ff0c762ff8a7a9e62309f7cc57d
jeppeSS <jeppeskov92@gmail.com>
2017-05-29 00:53:59 +0000

    Python script that creates bitmap

---
+++ src/test/bitmap.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import pylab as plt
+
+
+print("Drawing bitmap plot")
+with open("Data/bitmap.txt") as data:
+    dim = int(data.readline())
+    bitmap = np.ones((dim, dim), dtype=int)
+
+    x = 0
+    for line in data:
+        bits = np.fromstring(line, dtype=int, sep=',')
+        
+        bits = list(bits)
+
+        for y in range(len(bits)):
+            bitmap[x][y] = bits[y]
+
+        x += 1
+
+
+data.close()
+
+plt.imshow(bitmap, cmap='gray', interpolation='nearest')
+plt.xlabel('Pixel width')
+plt.ylabel('Pixel height')
+plt.title('Mersenne Twister Algorithm')
+plt.savefig('Data/bitmap.png')
