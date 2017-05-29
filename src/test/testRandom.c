d9e12b4f3387776ab428730ece208bfcf50dd303
jeppeSS <jeppeskov92@gmail.com>
2017-05-29 00:55:28 +0000

    Added further random test

--- src/test/testRandom.c
+++ src/test/testRandom.c
@@ -9,22 +9,26 @@ int main(void){
 
     printf("Testing random functions\n");
 
-    int numTest = 3;
+    int numTest = 4;
     int correct = 0;
 
     mpz_t randT1, rangT1;
     mpz_t randT2, rangT2;
     mpz_t randT3, rangT3;
+    mpz_t randT4, rangT4;
 
     mpz_inits(randT1, rangT1, NULL);
     mpz_inits(randT2, rangT2, NULL);
     mpz_inits(randT3, rangT3, NULL);
+    mpz_init(rangT4);
 
     mpz_set_ui(rangT1, 10);
     mpz_set_ui(rangT2, 1500000000);
     
     int base;
     base = mpz_set_str(rangT3, "150000000000000000000000000000000", 10);
+    
+    mpz_set_ui(rangT4, 1000);
 
     if(base < 0){
         fprintf(stderr, "[ERROR] Testing random failed\n");
@@ -38,7 +42,7 @@ int main(void){
         return EXIT_FAILURE;
     }
 
-    if(mpz_cmp_ui(randT1, 0) < 0 && mpz_cmp_ui(randT1, 11) > 0){
+    if(mpz_cmp_ui(randT1, 0) < 0 || mpz_cmp_ui(randT1, 11) > 0){
         fprintf(stdout, "Test 1: Failed\n");
     } else{
         correct++;
@@ -51,7 +55,7 @@ int main(void){
         return EXIT_FAILURE;
     }
     
-    if(mpz_cmp_ui(randT2, 0) < 0 && mpz_cmp_ui(randT2, 15000000001) > 0){
+    if(mpz_cmp_ui(randT2, 0) < 0 || mpz_cmp_ui(randT2, 15000000001) > 0){
         fprintf(stdout, "Test 2: Failed\n");
     } else{
         correct++;
@@ -65,20 +69,50 @@ int main(void){
     }
 
     
-    if(mpz_cmp_ui(randT3, 0) < 0 &&
+    if(mpz_cmp_ui(randT3, 0) < 0 ||
        mpz_cmp(randT3, rangT3) > 0){
         fprintf(stdout, "Test 3: Failed\n");
     } else{
         correct++;
     }
 
-    printf("Test result: %d/%d correct\n", correct, numTest);
+    
+    int ret4;
+    int flag = 0;
+    for(int i = 0; i < 2000; i++){
+        
+        mpz_init(randT4);
+
+        
+        ret4 = randomUniform(randT4, rangT4);
+        
+        if(ret4){
+            fprintf(stderr, "[ERROR] Testing random failed\n");
+            return EXIT_FAILURE;
+        }
+        
+        if(mpz_cmp_ui(randT4, 0) < 0 || mpz_cmp_ui(randT4, 1000) > 0){
+            flag = 1;
+        }
+
+        mpz_clear(randT4);
+
+    }
+
+    if(flag){
+        fprintf(stdout, "Test 3: Failed\n");
+    } else {
+        correct++;
+    }
+
 
 
     mpz_clears(randT1, rangT1, NULL);
     mpz_clears(randT2, rangT2, NULL);
     mpz_clears(randT3, rangT3, NULL);
-
+    mpz_clear(rangT4);
+    
+    printf("Test result: %d/%d correct\n", correct, numTest);
     return 0;
 }
 
