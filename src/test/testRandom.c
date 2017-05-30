cd7291b3c6db112c8932fb07a489a81287151164
jeppeSS <jeppeskov92@gmail.com>
2017-05-30 10:27:00 +0000

    Test randomRange function

--- src/test/testRandom.c
+++ src/test/testRandom.c
@@ -9,7 +9,7 @@ int main(void){
 
     printf("Testing random functions\n");
 
-    int numTest = 4;
+    int numTest = 6;
     int correct = 0;
 
     mpz_t randT1, rangT1;
@@ -100,17 +100,89 @@ int main(void){
     }
 
     if(flag){
-        fprintf(stdout, "Test 3: Failed\n");
+        fprintf(stdout, "Test 4: Failed\n");
     } else {
         correct++;
     }
 
+
+    mpz_t val1, min1, max1;
+    mpz_inits(min1, max1, NULL);
+
+    mpz_set_ui(min1, 50);
+    mpz_set_ui(max1, 100);
+
+    int ret5; 
+    flag = 0;
+    
+    for(int i = 0; i < 2000; i++){
+        
+        mpz_init(val1);
+        
+        ret5 = randomRange(val1, min1, max1);
+        
+        if(ret5){
+            fprintf(stderr, "[ERROR] Testing random failed\n");
+            return EXIT_FAILURE;
+        }
+        
+        if(mpz_cmp(val1, min1) < 0 || mpz_cmp(val1, max1) > 0){
+            flag = 1;
+        }
+
+
+        mpz_clear(val1);
+
+    }
+
+    if(flag){
+        fprintf(stdout, "Test 5: Failed\n");
+    } else {
+        correct++;
+    }
+    
+    mpz_t val2, min2, max2;
+    mpz_inits(min2, max2, NULL);
+
+    mpz_set_str(min2, "10000000000", 10);
+    mpz_set_str(max2, "150000000000000000000000000000000", 10);
+
+    int ret6; 
+    flag = 0;
+    
+    for(int i = 0; i < 200; i++){
+        
+        mpz_init(val2);
+        
+        ret6 = randomRange(val2, min2, max2);
+        
+        if(ret6){
+            fprintf(stderr, "[ERROR] Testing random failed\n");
+            return EXIT_FAILURE;
+        }
+        
+        if(mpz_cmp(val2, min2) < 0 || mpz_cmp(val2, max2) > 0){
+            flag = 1;
+        }
+
+
+        mpz_clear(val2);
+
+    }
+
+    if(flag){
+        fprintf(stdout, "Test 6: Failed\n");
+    } else {
+        correct++;
+    }
 
 
     mpz_clears(randT1, rangT1, NULL);
     mpz_clears(randT2, rangT2, NULL);
     mpz_clears(randT3, rangT3, NULL);
     mpz_clear(rangT4);
+    mpz_clears(min1, max1, NULL);
+    mpz_clears(min2, max2, NULL);
     
     printf("Test result: %d/%d correct\n", correct, numTest);
     return 0;
