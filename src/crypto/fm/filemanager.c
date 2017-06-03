86ae5c7bbe0cae981f038e51848a715d59b0d310
jeppeSS <jeppeskov92@gmail.com>
2017-06-03 11:31:33 +0000

    Added comments

--- src/crypto/fm/filemanager.c
+++ src/crypto/fm/filemanager.c
@@ -9,15 +9,26 @@
 #include "../publickey.h"
 
 
+/* 
+ * === Function ===============================================================
+ *         Name: writeSK
+ *
+ *  Description: Writes the secret key out to the pointed file.
+ * ============================================================================
+ */
 int writeSK(SK *sk, const char *filename){
 
+    // Open the file for writing
     FILE *fp = fopen(filename, "wb");
 
+    // Opening file failed
     if(!fp){
         fprintf(stderr, "[ERROR] Could not write secret key to file\n");
         return EXIT_FAILURE;
     }
     
+    // Contains the parameters for the secret key only lambda
+    // because the others parameters can be derived from lambda.
     const char *BEGINPAR = "-----BEGIN SHE PARAMATERS-----";
     const char *ENDPAR   = "-----END SHE PARAMATERS-----";
 
@@ -27,6 +38,7 @@ int writeSK(SK *sk, const char *filename){
 
     fprintf(fp, "%s\n", ENDPAR);
 
+    // Write the secret key to the file in base 16.
     const char *BEGINK = "-----BEGIN SHE SECRET KEY-----";
     const char *ENDK   = "-----END SHE SECRET KEY-----";
 
@@ -34,6 +46,7 @@ int writeSK(SK *sk, const char *filename){
 
     size_t data = mpz_out_str(fp, 16, sk->secK);
 
+    // Writing key failed
     if(!data){
         fprintf(stderr, "[ERROR] Could not write secret key\n");
         return EXIT_FAILURE;
@@ -42,6 +55,7 @@ int writeSK(SK *sk, const char *filename){
     fprintf(fp, "\n%s\n", ENDK);
 
 
+    // Close file
     fclose(fp);
 
 
